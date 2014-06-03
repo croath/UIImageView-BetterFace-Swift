6afab9cef81ab6217d960313e10edfed54215f17
croath <croathliu@gmail.com>
2014-06-04 01:42:55 +0800

    project init

---
+++ bfswift/bfswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bfswift
+//
+//  Created by croath on 6/4/14.
+//  Copyright (c) 2014 croath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bfswift/bfswiftTests/bfswiftTests.swift
@@ -0,0 +1,35 @@
+//
+//  bfswiftTests.swift
+//  bfswiftTests
+//
+//  Created by croath on 6/4/14.
+//  Copyright (c) 2014 croath. All rights reserved.
+//
+
+import XCTest
+
+class bfswiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
