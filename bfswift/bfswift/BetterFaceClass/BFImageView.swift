cde4447e8da90ce31bc5362e46f2b8fd9af66d9f
croath <croathliu@gmail.com>
2014-06-04 09:33:26 +0800

    subclass added.

---
+++ bfswift/bfswift/BetterFaceClass/BFImageView.swift
@@ -0,0 +1,145 @@
+//
+//  BFImageView.swift
+//  bfswift
+//
+//  Created by croath on 6/4/14.
+//  Copyright (c) 2014 croath. All rights reserved.
+//
+//
+import Foundation
+import UIKit
+import QuartzCore
+
+let BETTER_LAYER_NAME = "BETTER_LAYER_NAME"
+let GOLDEN_RATIO = 0.618
+
+class BFImageView: UIImageView {
+    var needsBetterFace: Bool = false
+    
+    var fast: Bool = true
+    
+    var detector: CIDetector?
+    
+    override var image: UIImage! {
+    get {
+        return super.image
+    }
+    set {
+        super.image = newValue
+        if self.needsBetterFace {
+            self.faceDetect(newValue)
+        }
+    }
+    }
+    
+    func faceDetect(aImage: UIImage){
+        var queue: dispatch_queue_t = dispatch_queue_create("com.croath.betterface.queue", DISPATCH_QUEUE_CONCURRENT)
+        dispatch_async(queue,
+            {
+                var image = aImage.CIImage
+                if image == nil {
+                    image = CIImage(CGImage: aImage.CGImage)
+                }
+                
+                if self.detector == nil {
+                    var opts = [(self.fast ? CIDetectorAccuracyLow : CIDetectorAccuracyHigh): CIDetectorAccuracy]
+                    self.detector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: opts)
+                }
+                
+                var features: AnyObject[] = self.detector!.featuresInImage(image)
+                
+                if features.count == 0 {
+                    println("no faces")
+                    dispatch_async(dispatch_get_main_queue(),
+                        {
+                            self.imageLayer().removeFromSuperlayer()
+                        })
+                } else {
+                    println("succeed \(features.count) faces")
+                    var imgSize = CGSizeMake(Float(CGImageGetWidth(aImage.CGImage)), Float(CGImageGetHeight(aImage.CGImage)))
+                    self.markAfterFaceDetect(features, size: imgSize)
+                }
+            })
+    }
+    
+    func markAfterFaceDetect(features: AnyObject[], size: CGSize) {
+        var fixedRect = CGRectMake(MAXFLOAT, MAXFLOAT, 0, 0)
+        var rightBorder: Float = 0, bottomBorder: Float = 0
+        for f: AnyObject in features {
+            var oneRect = CGRectMake(f.bounds.origin.x, f.bounds.origin.y, f.bounds.size.width, f.bounds.size.height)
+            oneRect.origin.y = size.height - oneRect.origin.y - oneRect.size.height
+            
+            fixedRect.origin.x = min(oneRect.origin.x, fixedRect.origin.x)
+            fixedRect.origin.y = min(oneRect.origin.y, fixedRect.origin.y)
+            
+            rightBorder = max(oneRect.origin.x + oneRect.size.width, rightBorder)
+            bottomBorder = max(oneRect.origin.y + oneRect.size.height, bottomBorder)
+        }
+        
+        fixedRect.size.width = rightBorder - fixedRect.origin.x
+        fixedRect.size.height = bottomBorder - fixedRect.origin.y
+        
+        var fixedCenter: CGPoint = CGPointMake(fixedRect.origin.x + fixedRect.size.width / 2.0,
+            fixedRect.origin.y + fixedRect.size.height / 2.0)
+        var offset: CGPoint = CGPointZero
+        var finalSize: CGSize = size
+        if size.width / size.height > self.bounds.size.width / self.bounds.size.height {
+            //move horizonal
+            finalSize.height = self.bounds.size.height
+            finalSize.width = size.width/size.height * finalSize.height
+            fixedCenter.x = finalSize.width / size.width * fixedCenter.x
+            fixedCenter.y = finalSize.width / size.width * fixedCenter.y
+            
+            offset.x = fixedCenter.x - self.bounds.size.width * 0.5
+            if (offset.x < 0) {
+                offset.x = 0
+            } else if (offset.x + self.bounds.size.width > finalSize.width) {
+                offset.x = finalSize.width - self.bounds.size.width
+            }
+            offset.x = -offset.x
+        } else {
+            //move vertical
+            finalSize.width = self.bounds.size.width
+            finalSize.height = size.height/size.width * finalSize.width
+            fixedCenter.x = finalSize.width / size.width * fixedCenter.x
+            fixedCenter.y = finalSize.width / size.width * fixedCenter.y
+            
+            offset.y = fixedCenter.y - self.bounds.size.height * Float(1-GOLDEN_RATIO)
+            if (offset.y < 0) {
+                offset.y = 0
+            } else if (offset.y + self.bounds.size.height > finalSize.height){
+                finalSize.height = self.bounds.size.height
+                offset.y = finalSize.height
+            }
+            offset.y = -offset.y
+        }
+        
+        dispatch_async(dispatch_get_main_queue(),
+            {
+                var layer: CALayer = self.imageLayer()
+                layer.frame = CGRectMake(offset.x, offset.y, finalSize.width, finalSize.height)
+                layer.contents = self.image.CGImage
+            })
+    }
+    
+    func imageLayer() -> CALayer {
+        if let sublayers = self.layer.sublayers {
+            for layer: AnyObject in sublayers {
+                if layer.name == BETTER_LAYER_NAME {
+                    return layer as CALayer
+                }
+            }
+        }
+        
+        var layer = CALayer()
+        layer.name = BETTER_LAYER_NAME
+        layer.actions =
+            [
+                "contents": NSNull(),
+                "bounds": NSNull(),
+                "position": NSNull()
+        ]
+        self.layer.addSublayer(layer)
+        return layer
+    }
+}
--- bfswift/bfswift/ViewController.swift
+++ bfswift/bfswift/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var view0 : UIImageView
+    @IBOutlet var view1 : BFImageView
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view0.layer.borderColor = UIColor.grayColor().CGColor
+        self.view0.layer.borderWidth = 0.5
+        self.view0.contentMode = UIViewContentMode.ScaleAspectFill
+        self.view0.clipsToBounds = true
+        
+        self.view1.layer.borderColor = UIColor.grayColor().CGColor
+        self.view1.layer.borderWidth = 0.5
+        self.view1.contentMode = UIViewContentMode.ScaleAspectFill
+        self.view1.clipsToBounds = true
+        self.view1.needsBetterFace = true
+        self.view1.fast = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func tabPressed(sender : AnyObject) {
+        var imageStr:String = ""
+        switch sender.tag {
+        case Int(0):
+            imageStr = "up1.jpg"
+        case Int(1):
+            imageStr = "up2.jpg"
+        case Int(2):
+            imageStr = "up3.jpg"
+        case Int(3):
+            imageStr = "up4.jpg"
+        case Int(4):
+            imageStr = "l1.jpg"
+        case Int(5):
+            imageStr = "l2.jpg"
+        case Int(6):
+            imageStr = "l3.jpg"
+        case Int(7):
+            imageStr = "l4.jpg"
+        case Int(8):
+            imageStr = "m1.jpg"
+        case Int(9):
+            imageStr = "m2.jpg"
+        default:
+            imageStr = ""
+        }
+        
+        self.view0.image = UIImage(named: imageStr)
+        self.view1.image = UIImage(named: imageStr)
 
+    }
 
 }
-
